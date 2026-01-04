2cc27ca305d01910b9b7e09843cb9acde54c6e09
rlauuzo <46294892+rlauuzo@users.noreply.github.com>
2026-01-04 15:20:10 +0100

    Initial commit

---
+++ SegmentEditorPlugin/Controllers/SegmentEditorController.cs
@@ -0,0 +1,67 @@
+using System.Collections.Frozen;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SegmentEditorPlugin.Controllers;
+
+/// <summary>
+/// Controller for segment editor operations.
+/// </summary>
+[Route("SegmentEditor")]
+[ApiController]
+public class SegmentEditorController : ControllerBase
+{
+    private static readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+
+    private static readonly FrozenDictionary<string, string> _contentTypes =
+        new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            [".js"] = "application/javascript",
+            [".css"] = "text/css",
+            [".woff2"] = "font/woff2",
+            [".woff"] = "font/woff",
+            [".ttf"] = "font/ttf",
+            [".svg"] = "image/svg+xml",
+            [".png"] = "image/png",
+            [".jpg"] = "image/jpeg",
+            [".jpeg"] = "image/jpeg",
+            [".json"] = "application/json"
+        }.ToFrozenDictionary();
+
+    /// <summary>
+    /// Gets main index page.
+    /// </summary>
+    /// <response code="200">Main index page retrieved.</response>
+    /// <returns>The action result.</returns>
+    [HttpGet]
+    [HttpGet("index.html")]
+    [ResponseCache(Duration = 86400)]
+    public ActionResult GetIndex()
+    {
+        var stream = _assembly.GetManifestResourceStream("SegmentEditorPlugin.dist.index.html");
+        return stream == null ? NotFound() : new FileStreamResult(stream, "text/html");
+    }
+
+    /// <summary>
+    /// Gets embedded static resources from assets folder.
+    /// </summary>
+    /// <param name="path">The resource path.</param>
+    /// <returns>The action result.</returns>
+    [HttpGet("assets/{**path}")]
+    [ResponseCache(Duration = 86400)]
+    public ActionResult GetAssets(string path)
+    {
+        ArgumentNullException.ThrowIfNull(path);
+
+        var resourcePath = $"SegmentEditorPlugin.dist.assets.{path.Replace('/', '.')}";
+        var stream = _assembly.GetManifestResourceStream(resourcePath);
+
+        if (stream == null)
+        {
+            return NotFound();
+        }
+
+        var contentType = _contentTypes.GetValueOrDefault(Path.GetExtension(path), "application/octet-stream");
+        return new FileStreamResult(stream, contentType);
+    }
+}
---
+++ SegmentEditorPlugin/Plugin.cs
@@ -0,0 +1,50 @@
+using System.Globalization;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using SegmentEditorPlugin.Configuration;
+
+namespace SegmentEditorPlugin;
+
+/// <summary>
+/// The main plugin.
+/// </summary>
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Plugin"/> class.
+    /// </summary>
+    /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
+    /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    /// <inheritdoc />
+    public override string Name => "SegmentEditor";
+
+    /// <inheritdoc />
+    public override Guid Id => Guid.Parse("ace21d44-a4e5-4a85-ae75-acd2e24a9574");
+
+    /// <summary>
+    /// Gets the current plugin instance.
+    /// </summary>
+    public static Plugin? Instance { get; private set; }
+
+    /// <inheritdoc />
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
+            {
+                Name = Name,
+                EnableInMainMenu = true,
+                EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
+            }
+        ];
+    }
+}
